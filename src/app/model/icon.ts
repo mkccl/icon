4e2b863370d95b702ab4802390266632422c532f
Michael Krause <78827985+mkccl@users.noreply.github.com>
2022-06-08 23:54:10 +0200

    head

---
+++ src/app/model/icon.ts
@@ -0,0 +1,10 @@
+export class Icon {
+  className!: string;
+  isOpacity!: string;
+
+
+  constructor(className: string, isOpacity: string) {
+    this.className = className;
+    this.isOpacity = isOpacity;
+  }
+}
